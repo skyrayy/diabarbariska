c68901c8369fc0f0ac050e083fd154e4a8871896
skyrayy <sskyy.rrayy@gmail.com>
2025-05-08 20:16:14 +0300

    Add files via upload

--- diary-main/main.py
+++ diary-main/main.py
@@ -1,5 +1,5 @@
 #Импорт
-from flask import Flask, render_template,request, redirect
+from flask import Flask, render_template, request, redirect, session
 #Подключение библиотеки баз данных
 from flask_sqlalchemy import SQLAlchemy
 
@@ -9,38 +9,95 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///diary.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #Создание db
-db = SQLAlchemy(app )
+db = SQLAlchemy(app)
 
-#Задание №1. Создай таблицу БД
+app.secret_key = 'my_top_secret_123'
+#Создание таблицы
 
 class Card(db.Model):
+    #Создание полей
+    #id
+    id = db.Column(db.Integer, primary_key=True)
+    #Заголовок
     title = db.Column(db.String(100), nullable=False)
-    id = db.Column(db.Integer,primary_key=True)
-    subtitle = db.Column(db.String, nullable=False)
+    #Описание
+    subtitle = db.Column(db.String(300), nullable=False)
+    #Текст
     text = db.Column(db.Text, nullable=False)
+
+    #Вывод объекта и id
     def __repr__(self):
-            return f'<Card {self.id}>'
+        return f'<Card {self.id}>'
+    
+
+#Задание №1. Создать таблицу User
+
+class User(db.Model):
+    #создание полей
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(30), nullable=False)
+    password = db.Column(db.String(30), nullable=False)
+
+
+
 
 
 
 
 #Запуск страницы с контентом
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
+def login():
+        error = ''
+        if request.method == 'POST':
+            form_login = request.form['email']
+            form_password = request.form['password']
+            
+            #Задание №4. Реализовать проверку пользователей
+            
+            user_db=User.query.all()
+
+            for user in user_db:
+                if form_login == user.email and form_password == user.password:
+                    session['user_email'] = user.email
+                    return redirect('/index')
+                error = 'Неправильно указан пользователь или пароль'
+                return render_template('login.html', error=error)
+
+            
+        else:
+            return render_template('login.html')
+
+
+
+@app.route('/reg', methods=['GET','POST'])
+def reg():
+    if request.method == 'POST':
+        login= request.form['email']
+        password = request.form['password']
+        
+        #Задание №3. Реализовать запись пользователей
+        
+        user = User(email=login, password=password)
+        db.session.add(user)
+        db.session.commit()
+        
+        return redirect('/')
+    
+    else:    
+        return render_template('registration.html')
+
+
+#Запуск страницы с контентом
+@app.route('/index')
 def index():
     #Отображение объектов из БД
-    #Задание №2. Отоброзить объекты из БД в index.html
-    cards=Card.query.order_by(Card.id).all()
-
-    return render_template('index.html',
-                           cards = cards
-
-                           )
+    cards = Card.query.order_by(Card.id).all()
+    return render_template('index.html', cards=cards)
 
 #Запуск страницы c картой
 @app.route('/card/<int:id>')
 def card(id):
-    #Задание №2. Отоброзить нужную карточку по id
-    card=Card.query.get(id)
+    card = Card.query.get(id)
 
     return render_template('card.html', card=card)
 
@@ -57,18 +114,16 @@ def form_create():
         subtitle =  request.form['subtitle']
         text =  request.form['text']
 
-        #Задание №2. Создайте сопосб записи данных в БД
-        card = Card(title=title,subtitle=subtitle,text=text)
+        #Создание объкта для передачи в дб
+
+        card = Card(title=title, subtitle=subtitle, text=text)
 
         db.session.add(card)
-       
-
         db.session.commit()
-
-
-        return redirect('/')
+        return redirect('/index')
     else:
         return render_template('create_card.html')
+
 
 
 if __name__ == "__main__":
